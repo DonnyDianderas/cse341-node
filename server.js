559cc6f90fceea1fe48abc680a07875cc1580414
DonnyDianderas <donnydianderas@gmail.com>
2025-10-29 22:42:17 -0500

    Connect node project to MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongodb = require('./data/contacts');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
-app.listen(port, () => {console.log(`Runnig on port ${port}`)});
+
+mongodb.initDb((err)=> {
+  if (err){
+    console.log(err);
+  }
+  else{
+    app.listen(port, () => {console.log(`Database is listening and node Runnig on port ${port}`)});
+  }
+});
+
